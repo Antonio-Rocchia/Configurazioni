166bfe6e64e7d3c19600d9c30cf38929d8bfcf6c
Antonio Rocchia <tonino.rocchia@gmail.com>
2024-02-04 16:37:48 +0100

    Migrate to new neovim config via submodule

--- neovim/.config/nvim/lua/plugins/core/conveniences/mini.lua
+++
@@ -1,9 +0,0 @@
-return {
-  'echasnovski/mini.nvim',
-  version = false,
-  event = { "BufReadPost", "BufNewFile" },
-  config = function ()
-    require("mini.pairs").setup()
-    require("mini.comment").setup()
-  end
-}
