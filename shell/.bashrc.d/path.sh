22c2cc6b7bacecb38b0e6f862e9d6a5a0ff50fe3
Antonio Rocchia <tonino.rocchia@gmail.com>
2024-03-17 00:01:04 +0100

    Update path and self-completition for new script repository

--- shell/.bashrc.d/path.sh
+++ shell/.bashrc.d/path.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+# shellcheck disable=2076
 
 declare -a user_path_dirs=(
   "${HOME}/.local/bin" \
-  "${HOME}/.local/bin/config_managed"
+  "${HOME}/.local/bin/config_managed" \
+  "${HOME}/.local/bin/config_managed/scripts" 
 )
 
 for dir in "${user_path_dirs[@]}"; do
--- shell/.bashrc.d/self-completition.sh
+++ shell/.bashrc.d/self-completition.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 #
 # Enable command completition for various scripts
+shopt -s globstar nullglob
 
+declare script
 
-for f in "${HOME}"/.local/bin/config_managed/*; do
-  declare script
-  script=$(basename "${f}")
+for file in "${HOME}"/.local/bin/config_managed/**/*; do
+  script=$(basename "${file}")
   complete -C "${script}" "${script}"
 done
 
 unset script
-unset f
+unset file
