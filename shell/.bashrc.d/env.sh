7f71d9a4efafa451d5dd9095c9cfecd2a1cdb81d
Antonio-Rocchia <tonino.rocchia@gmail.com>
2024-03-18 11:30:26 +0100

    Combine path.sh and env.sh. Don't make too many files with similar purpouse.

--- shell/.bashrc.d/env.sh
+++ shell/.bashrc.d/env.sh
@@ -1,3 +1,24 @@
 #!/bin/bash
+# shellcheck disable=2076
 
 export EDITOR=/usr/bin/nvim
+
+#############################
+# Path
+#############################
+declare -a user_path_dirs=(
+  "${HOME}/.local/bin" \
+  "${HOME}/.local/bin/config_managed" \
+  "${HOME}/.local/bin/config_managed/scripts" 
+)
+
+for dir in "${user_path_dirs[@]}"; do
+  if ! [[ "${PATH}" =~ "${dir}" ]]; then
+    PATH="${dir}:${PATH}"
+  fi
+done
+
+unset user_path_dirs
+
+export PATH
+
